85b4b299840edae573206c0a671ef2164dc32232
AsTroZ02 <130462368+AsTroZ02@users.noreply.github.com>
2024-09-09 22:17:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggle-button');
+    const navMenu = document.getElementById('nav-menu');
 
+    toggleButton.addEventListener('click', function() {
+        if (navMenu.style.display === 'block') {
+            navMenu.style.display = 'none';
+        } else {
+            navMenu.style.display = 'block';
+        }
+    });
+});
